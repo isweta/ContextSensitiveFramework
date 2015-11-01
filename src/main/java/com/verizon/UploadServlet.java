d61d1619156a7d538f4eb077cee3f9dc68f7f6f3
isweta <mailbox.sweta@gmail.com>
2015-10-31 23:02:44 -0700

    Update UploadServlet.java

--- src/main/java/com/verizon/UploadServlet.java
+++ src/main/java/com/verizon/UploadServlet.java
@@ -1,7 +1,12 @@
 package com.verizon;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.PrintWriter;
 import java.util.Collection;
 
 import javax.servlet.ServletException;
@@ -14,67 +19,120 @@ import javax.servlet.http.HttpSession;
 import javax.servlet.http.Part;
 
 @WebServlet("/UploadServlet")
-@MultipartConfig(fileSizeThreshold=1024*1024*2, // 2MB 
-maxFileSize=1024*1024*10,      // 10MB
-maxRequestSize=1024*1024*50)   // 50MB
+@MultipartConfig(fileSizeThreshold = 1024 * 1024 * 2, // 2MB
+maxFileSize = 1024 * 1024 * 10, // 10MB
+maxRequestSize = 1024 * 1024 * 50) // 50MB
 public class UploadServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-	 private static final String SAVE_DIR = "uploadFiles";
-    
-    public UploadServlet() {
-        super();
-        
-    }
+	private static final String SAVE_DIR = "uploadFiles";
 
-	
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
-		
-        String appPath = request.getServletContext().getRealPath("/");
-        System.out.println("ap path is:"+appPath);
-        String savePath = appPath + File.separator + SAVE_DIR;
-		
-		
-        File fileSaveDir = new File(savePath);
-        if (!fileSaveDir.exists()) {
-            fileSaveDir.mkdir();
-        }
-        String fileName=null ;
-        Collection<Part> parts = request.getParts();
-        for (Part part : parts) {
-        	fileName = extractFileName(part);
-        	 // part.write(savePath +File.separator+ fileName);
-           part.write(fileName);
-        }
-        System.out.println(fileName);
-       
-		String filePath=ReadWriteTextFileJDK7.mainReadWrite(fileName, savePath);
-		HttpSession session=request.getSession();
+	public UploadServlet() {
+		super();
+
+	}
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		String appPath = request.getServletContext().getRealPath("/");
+		System.out.println("ap path is:" + appPath);
+		String savePath = appPath + File.separator + SAVE_DIR;
+
+		File fileSaveDir = new File(savePath);
+		if (!fileSaveDir.exists()) {
+			fileSaveDir.mkdir();
+		}
+		String fileName = null;
+		Collection<Part> parts = request.getParts();
+		for (Part part : parts) {
+			fileName = extractFileName(part);
+			// part.write(savePath +File.separator+ fileName);
+			part.write(fileName);
+		}
+		System.out.println(fileName);
+
+		String filePath = ReadWriteTextFileJDK7.mainReadWrite(fileName, savePath);
+		HttpSession session = request.getSession();
 		session.setAttribute("filePath", filePath);
-        request.setAttribute("message", "Upload has been done successfully!");
-        getServletContext().getRequestDispatcher("/FieldsListServlet").forward(
-                request, response);
+		request.setAttribute("message", "Upload has been done successfully!");
+		getServletContext().getRequestDispatcher("/FieldsListServlet").forward(request, response);
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		doGet(request, response);
+		// response.setContentType("text/html;charset=UTF-8");
+
+		// Create path components to save the file
+		String appPath = request.getServletContext().getRealPath("/");
+		String savePath = appPath +  "uploadFiles";
+		final String path = savePath;
+		final Part filePart = request.getPart("file");
+		final String fileoldPath = extractFileName(filePart);
+		int ind=fileoldPath.lastIndexOf("\\");
+	    if (ind==-1)
+	    	ind=fileoldPath.lastIndexOf("/");
+	    
+	    
+	    String fileName = fileoldPath.substring(ind+1);
+		System.out.println("path :"+path);
+		System.out.println("filename: "+fileName);
+		OutputStream out = null;
+		InputStream filecontent = null;
+		// final PrintWriter writer = response.getWriter();
+
+		try {
+			out = new FileOutputStream(new File(path + File.separator + fileName));
+			filecontent = filePart.getInputStream();
+
+			int read = 0;
+			final byte[] bytes = new byte[1024];
+
+			while ((read = filecontent.read(bytes)) != -1) {
+				out.write(bytes, 0, read);
+			}
+			// writer.println("New file " + fileName + " created at " + path);
+			HttpSession session = request.getSession();
+			String filePath = path + File.separator + fileName;
+			session.setAttribute("filePath", filePath);
+			request.setAttribute("message", "Upload has been done successfully!");
+			getServletContext().getRequestDispatcher("/FieldsListServlet").forward(request, response);
+
+		} catch (FileNotFoundException fne) {
+			// writer.println("You either did not specify a file to upload or
+			// are "
+			// + "trying to upload a file to a protected or nonexistent "
+			// + "location.");
+			// writer.println("<br/> ERROR: " + fne.getMessage());
+			fne.printStackTrace();
+
+		} finally {
+			if (out != null) {
+				out.close();
+			}
+			if (filecontent != null) {
+				filecontent.close();
+			}
+			// if (writer != null) {
+			// writer.close();
+		}
 	}
-	
+
 	private String extractFileName(Part part) {
-        String contentDisp = part.getHeader("content-disposition");
-        String[] items = contentDisp.split(";");
-        for (String s : items) {
-            if (s.trim().startsWith("filename")) {
-            	//(s.indexOf('=') + 1).trim().replace("\"", "")
-            	//actual (s.indexOf("=") + 2, s.length()-1)
-                return s.substring(s.indexOf("=") + 2, s.length()-1);
-            }
-        }
-        return "";
-    }
+		final String partHeader = part.getHeader("content-disposition");
+	    
+	    for (String content : part.getHeader("content-disposition").split(";")) {
+	        if (content.trim().startsWith("filename")) {
+	            return content.substring(
+	                    content.indexOf('=') + 1).trim().replace("\"", "");
+	        }
+	    }
+	    return null;
+	}
 
 }
+
